b226dbb1512ffe019a00a5cd21187185289e9488
Gayuth-W <gayuthdemika@gmail.com>
2025-02-07 20:17:59 +0530

    Update gpsdata.php

--- gpsdata.php
+++ gpsdata.php
@@ -1,14 +1,8 @@
 <?php 
-
 require 'config.php';
-
-//----------------------------------------------------------------------------
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$api_key = escape_data($_POST["api_key"]);
-        //print_r($_POST);
-        //echo "<br>ESP32_API_KEY: ".ESP32_API_KEY."<br>";
-	//MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
 	if($api_key == ESP32_API_KEY) {
 		$latitude = escape_data($_POST["lat"]);
 		$longitude = escape_data($_POST["lng"]);
@@ -22,20 +16,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		echo "OK. INSERT ID: ";
 		echo $db->insert_id;
 	}
-	//MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
 	else
 	{
 		echo "Wrong API Key";
 	}
-	//MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
 }
-//----------------------------------------------------------------------------
 else
 {
 	echo "No HTTP POST request found";
 }
-//----------------------------------------------------------------------------
-
 
 function escape_data($data) {
     $data = trim($data);
